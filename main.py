60fcf95e0ef03f832a21db5fcccae41d80fa071c
Rafael B Ferreira <rafael2517z@gmail.com>
2021-03-29 17:09:41 -0300

    [CHG] Algoritmo funcional

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import os
 from PIL import Image
 
 
+#Guarda os nomes das imgs em uma lista
+my_imgs = []
+for root, dirs, files in os.walk("imgs"):
+    for filename in files:
+        my_imgs.append(filename)
+
 #pytesseract.pytesseract.tesseract_cmd = os.environ['TESSERACT']
 
+#Importa o tesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-phrase = pytesseract.image_to_string(Image.open('test.jpeg'), lang='por')
-print(phrase)
+#Caminho das imgs
+path = 'imgs/'
+
+#Passa o tesseract nas imagens da pasta img
+for img in my_imgs:
+    phrase = pytesseract.image_to_string(Image.open(path + img), lang='por')
+    print(img + "--->" + phrase)
