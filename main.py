a6584f4844d95364930e90799b6f86ca1b10cdeb
Rafael B Ferreira <rafael2517z@gmail.com>
2021-08-17 23:04:22 -0300

    [ADD] Função que retorna a confidence

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
+from pytesseract import pytesseract,Output
 from PIL import Image
-import pytesseract 
-import os
-import pandas as pd
 
 
-#Importa o tesseract
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+def get_confidence(image_path,lang):
+    
+    texto = pytesseract.image_to_data(Image.open(image_path),lang=lang,output_type=Output.DICT)
 
-#Dicionario para guardar o resultado das imgs
-my_dict = {}
+    result = {'TEXTO':[],'ACURACIA':[]}
+    for i in range(len(texto['text'])):
+        if len(texto['text'][i]) > 0:
+            #print('TEXTO: {}\nACURÁCIA: {}\n'.format(texto['text'][i],texto['conf'][i]))
 
-#Passa o tesseract nas imagens da pasta imgs e salva o resultado no dicionario
-for root, dirs, files in os.walk("imgs"):
-    for filename in files:
-        try:
-            texto = pytesseract.image_to_string(Image.open('imgs/' + filename), lang='por')
-            my_dict[filename] = texto
-        except Exception as msg:
-            print("Erro: {}".format(msg))
-        
-#Transforma o dicionário em um dataframe
-df = pd.DataFrame(my_dict.items(), columns=['NOME_IMG', 'TEXTO_EXT'])
+            result['TEXTO'].append(texto['text'][i])
+            result['ACURACIA'].append(texto['conf'][i])
+            
+    return result
 
-#Transforma dataframe em um .csv
-df.to_csv('dataframe.csv', encoding="utf-8")
+a = get_confidence('./imgs/img3.jpg','por')
+b = get_confidence('./imgs/img3.jpg','eng')
 
-print(df)
+print(a['ACURACIA'])
+print(b['ACURACIA'])
