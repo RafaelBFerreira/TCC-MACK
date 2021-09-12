52eb13ddd0530e137f00d28e42f0a3463c49d979
Rafael B Ferreira <rafael2517z@gmail.com>
2021-09-12 18:59:13 -0300

    [CHG] Adequações

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 from pytesseract import pytesseract,Output
 from PIL import Image
+import pandas as pd
+
 
 
 def get_confidence(image_path,lang):
-    
-    texto = pytesseract.image_to_data(Image.open(image_path),lang=lang,output_type=Output.DICT)
+    img  = Image.open(image_path)
+    texto = pytesseract.image_to_data(img, lang=lang,output_type=Output.DICT)
 
-    result = {'TEXTO':[],'ACURACIA':[]}
+    result = {}
+
     for i in range(len(texto['text'])):
         if len(texto['text'][i]) > 0:
-            #print('TEXTO: {}\nACURÁCIA: {}\n'.format(texto['text'][i],texto['conf'][i]))
-
-            result['TEXTO'].append(texto['text'][i])
-            result['ACURACIA'].append(texto['conf'][i])
+            #print('TEXTO: {}\CONFIDENCIA: {}\n'.format(texto['text'][i],texto['conf'][i]))
             
-    return result
+            result[texto['text'][i]] = texto['conf'][i]
 
-a = get_confidence('./imgs/img3.jpg','por')
-b = get_confidence('./imgs/img3.jpg','eng')
+    #Transforma o dicionário em um dataframe
+    df_result = pd.DataFrame(result.items(),columns=['TEXTO', 'CONFIDENCIA'])
 
-print(a['ACURACIA'])
-print(b['ACURACIA'])
+    #Transforma dataframe em um .csv
+    df_result.to_csv('dataframe.csv', encoding="utf-8")
+
+    return df_result
+
+
+a = get_confidence('./imgs/img5.png','por.rafael')
+
+print(a)
+
+
+
+
+'''a = get_confidence('./imgs/img.png','por')
+b = get_confidence('./imgs/img.png','por.raff+por')
+c = get_confidence('./imgs/img.png','por+por.raff')
+d = get_confidence('./imgs/img.png','por.raff')
+
+print(a['TEXTO'],a['CONFIDENCIA'])
+print(b['TEXTO'],b['CONFIDENCIA'])
+print(c['TEXTO'],c['CONFIDENCIA'])
+print(d['TEXTO'],d['CONFIDENCIA'])'''
+
